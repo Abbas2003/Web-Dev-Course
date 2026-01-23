29ccb71f6c706e7d862b2fafe1a47867eff9545d
Abbas2003 <abbas.mohammad805@gmail.com>
2026-01-23 20:26:17 +0500

    learned alert and variables

---
+++ JS/Class_1/main.js
@@ -0,0 +1,22 @@
+// console.log("Hello World!");
+// alert("Hello World!");
+// alert("Fatima", "Rabiya", "Eshal");
+// window.alert("Excited to learn Javascript!");
+// console.log("Fatima", "Rabiya", "Eshal");
+
+// Variables in JavaScript
+// Variable Declaration
+var age;
+var university;
+university = "SMIU";
+console.log(university);
+
+// Variable Initialization
+var name = "Abbas";
+var nationality = "Irani"; // --> as reference "nationality"
+console.log(name);
+name = "Ahmed";
+console.log(name);
+
+alert(name);
+// var alert = "This is alert variable"; // Never use keywords as variable names 
